4181f4067195647e96fbc282b4d8565806db426d
Reganaselvaranjan <reganajf0325@gmail.com>
2024-08-20 09:14:03 +0530

    socket.io init

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-const app = require("./app")
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const app = require("./app");
+const { verifyJWT } = require('./utils/middlewares/jwtAuth');
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const http = createServer(app);
+const io = new Server(http, {
+
+});
+
+io.use((socket, next) => {
+    if (socket.recovered) {
+        console.log('Recovery');
+    }
+    const payload = verifyJWT(socket.request, next);
+    socket.
+});
+
+io.on('connection', (socket) => {
+    console.log(`Socket connected : ${socket.id}`);
+    console.log('headers', socket.handshake.headers);
+
+
+    socket.emit('welcome', `Hello user, ${socket.id}`);
+
+    socket.on('message', async (message, to) => {
+        console.log(`message : ${message}`);
+
+        socket.to(to).emit('message', `${socket.id} sent message :${message}`);
+        // socket.broadcast.emit('message', `${socket.id} sent message :${message}`);
+
+        console.log('Socket Data :', {
+            rooms: socket.rooms,
+        });
+        console.log('Io Data :', {
+            sockets1: io.sockets,
+            sockets2: await io.fetchSockets(),
+        });
+
+    });
+});
+
+http.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
 
--- utils/middlewares/jwtAuth.js
+++ utils/middlewares/jwtAuth.js
@@ -3,37 +3,42 @@ const AppError = require("../AppError");
 const { STATUS_CODE } = require("../constants");
 const jwt = require("jsonwebtoken");
 
-module.exports = async (req, res, next) => {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return next(new AppError("Invalid authorization header", STATUS_CODE.UNAUTHORIZED));
-    }
-
-    const token = authHeader.split(" ").pop();
-    if (!token) {
-        return next(new AppError("Authorization token not found", STATUS_CODE.UNAUTHORIZED));
-    }
-
+module.exports.verifyJWT = (req, next) => {
     try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return next(new AppError("Invalid authorization header", STATUS_CODE.UNAUTHORIZED));
+        }
+
+        const token = authHeader.split(" ").pop();
+        if (!token) {
+            return next(new AppError("Authorization token not found", STATUS_CODE.UNAUTHORIZED));
+        }
+
         console.log(token);
         const payload = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(`payload`, payload);
 
-        const user = await User.findOne({
-            where: {
-                user_id: payload.user_id,
-            },
-        });
-
-        if (!user) {
-            return next(new AppError("Invalid Token", STATUS_CODE.UNAUTHORIZED));
-        }
-        req.auth = user;
-
-        next();
+        return payload;
     } catch (error) {
         console.log(error);
         next(new AppError(error.name, STATUS_CODE.UNAUTHORIZED));
     }
+}
+
+module.exports = async (req, res, next) => {
+    console.log(`route URL ${req.originalUrl}`);
+
+    const payload = this.verifyJWT(req, next);
+
+    const user = await User.findOne({
+        where: {
+            user_id: payload.user_id,
+        },
+    });
+
+    if (!user) {
+        return next(new AppError("Invalid Token", STATUS_CODE.UNAUTHORIZED));
+    }
+    req.auth = user;
+    next();
 };
