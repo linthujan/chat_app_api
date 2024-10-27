f8819282810c95946f51858dbab5d2a4cf38a210
Linthu J <53619838+linthujan@users.noreply.github.com>
2024-10-27 13:18:13 +0530

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ http.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
 
-if (process.env.NODE_ENV != 'production') {
+if (process.env.NGROK == 'production') {
     const ngrok = require('@ngrok/ngrok');
     ngrok.connect({ addr: port, authtoken_from_env: true, domain: process.env.NGROK_DOMAIN })
         .then((listener) => console.log(`Ingress established at: ${listener.url()}`));
